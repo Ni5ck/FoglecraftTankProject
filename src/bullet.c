f882b390cb6b53017158dde6bae815bee30fc93e
Zachary Neumeier <zachary.neumeier@matrixresearch.com>
2021-04-11 19:35:10 -0400

    Created method to set direction vector of bullet

--- src/bullet.c
+++ src/bullet.c
@@ -1,5 +1,10 @@
 #include "bullet.h"
 
+/**
+ * Initiate bullet's starting position by setting it to the player's position
+ *
+ * @param player Structure containing a player's attributes.
+ */
 void init_bullet_position(struct Player *player) {
   State *state = &player->state;
   Bullet *bullet = &player->bullet;
@@ -7,4 +12,16 @@ void init_bullet_position(struct Player *player) {
   bullet->y = (float) state->y;
   bullet->z = (float) state->z;
   bullet->visible = true;
+}
+
+/**
+ * Set the direction vector of the bullet based on the sight vector of the player
+ * when the bullet is shot.
+ *
+ * @param player Structure containing a player's attributes.
+ */
+void set_bullet_flight_vector(Player *player) {
+    State *state = &player->state;
+    Bullet *bullet = &player->bullet;
+    get_sight_vector(state->rx, state->ry, &bullet->dirX, &bullet->dirY, &bullet->dirZ);
 }
--- src/bullet.h
+++ src/bullet.h
@@ -5,16 +5,20 @@
 
 struct Player;
 
+/**
+ * A structure to represent a bullet that a player shoots.
+ */
 typedef struct {
-    float x;
-    float y;
-    float z;
-    float dirX;
-    float dirY;
-    float dirZ;
-    bool visible;
+    float x; /**< x coordinate of bullet */
+    float y; /**< y coordinate of bullet */
+    float z; /**< z coordinate of bullet */
+    float dirX; /**< x value of direction vector */
+    float dirY; /**< y value of direction vector*/
+    float dirZ; /**< z value of direction vector */
+    bool visible; /**< flag indicating the bullet has been shot */
 } Bullet;
 
 void init_bullet_position(struct Player *player);
+void set_bullet_flight_vector(struct Player *player);
 
 #endif // _bullet_h
